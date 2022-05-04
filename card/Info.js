8892a2f31bbd4a4c7fb719d82a0d1e40513f8826
JessicaSilva039 <jessicaSilva0390@gmail.com>
2022-05-04 15:47:45 -0400

    Add files via upload
    Third layout option

--- card/Info.js
+++ card/Info.js
@@ -7,10 +7,9 @@ export default function Info(){
             <h3 className="name">Laura Smith</h3>
             <h4 className="job">Frontend Developer</h4>
             <h4 className="site">laurasmith.website</h4>
-        
+            
             <div className="buttons">
-        <span className="email"><span className="emailicon"></span>Email</span>
-        <span className="linkedin"><span className="linkedinicon"></span>LinkedIn</span>
+                <span className="email"><span className="emailicon"></span>Email</span>
             </div>
         </div>
     )
